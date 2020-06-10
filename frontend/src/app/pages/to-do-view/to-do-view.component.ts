0679760ab85183cfafe65d0eab514ce005878e92
Daniel Velasquez <apu.danielvelasquez@gmail.com>
2020-06-10 20:57:32 -0300

    Tomo las listas

--- frontend/src/app/pages/to-do-view/to-do-view.component.ts
+++ frontend/src/app/pages/to-do-view/to-do-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ToDoService } from 'src/app/services/to-do.service';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-to-do-view',
@@ -7,7 +8,18 @@ import { ToDoService } from 'src/app/services/to-do.service';
   styleUrls: ['./to-do-view.component.scss'],
 })
 export class ToDoViewComponent implements OnInit {
-  constructor(private todoService: ToDoService) {}
+  lists: any;
+  constructor(
+    private todoService: ToDoService,
+    private route: ActivatedRoute
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      console.log(params);
+    });
+    this.todoService.getLists().subscribe((lists: any[]) => {
+      this.lists = lists;
+    });
+  }
 }
