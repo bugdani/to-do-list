416101c62974ba68e4a0728af7668a0e8a4cbfb1
Daniel Velasquez <apu.danielvelasquez@gmail.com>
2020-06-10 11:24:12 -0300

    Creo los metodos

--- api/app.js
+++ api/app.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hola Mundo!!");
-});
+/**
+ * Get /lists
+ * Proposito: Devuelve todas las listas
+ */
+app.get("/lists", (req, res) => {});
+
+/**
+ * Post /lists
+ * Proposito: Para crear una nueva lista
+ */
+app.post("/lists", (req, res) => {});
+
+/**
+ * Path /lists
+ * Proposito: Para modificar una lista especifica
+ */
+app.path("/lists/:id", (req, res) => {});
 
 app.listen(3000, () => {
     console.log("El servidor ya esta escuchando peticiones!! ;) ");
