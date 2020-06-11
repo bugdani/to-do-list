dd6520b7b271cb114f60a7f3659ba9a0317550d9
Daniel Velasquez <apu.danielvelasquez@gmail.com>
2020-06-10 22:58:59 -0300

    Agrego Ruta del componente

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ToDoViewComponent } from './pages/to-do-view/to-do-view.component';
 import { NewListComponent } from './pages/new-list/new-list.component';
+import { NewTaskComponent } from './pages/new-task/new-task.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/lists', pathMatch: 'full' },
   { path: 'lists', component: ToDoViewComponent },
   { path: 'lists/:listId', component: ToDoViewComponent },
   { path: 'new-list', component: NewListComponent },
+  { path: 'new-task', component: NewTaskComponent },
 ];
 
 @NgModule({
