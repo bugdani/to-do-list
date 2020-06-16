5c8583f1d8d59823eec7558b079fec4145ed01ac
Daniel Velasquez <apu.danielvelasquez@gmail.com>
2020-06-15 21:59:44 -0300

    arreglo estilo de la busqueda

--- api/db/mongoose.js
+++ api/db/mongoose.js
@@ -2,22 +2,27 @@
 
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
+//mongodb+srv://<username>:<password>@bugdanidb-todo-2yvce.mongodb.net/<dbname>?retryWrites=true&w=majority
+//mongodb://localhost:27017/ToDoManager
 mongoose
-    .connect("mongodb://localhost:27017/ToDoManager", {
-        useNewUrlParser: true,
-    })
-    .then(() => {
-        console.log("Conexion establecida con MongoDB");
-    })
-    .catch((err) => {
-        console.log("Hubo un error en la conexion a MongoDB");
-        console.log(err);
-    });
+  .connect(
+    "mongodb+srv://bugdani:Rengo73@bugdanidb-todo-2yvce.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+    }
+  )
+  .then(() => {
+    console.log("Conexion establecida con MongoDB");
+  })
+  .catch((err) => {
+    console.log("Hubo un error en la conexion a MongoDB");
+    console.log(err);
+  });
 
 //Para que no me aparezcan advertencias innecesarias por deprecation
 mongoose.set("useCreateIndex", true);
 mongoose.set("useFindAndModify", false);
 
 module.exports = {
-    mongoose,
+  mongoose,
 };
